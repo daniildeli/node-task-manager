aeff83e36b8c5aaa5e32fafa36cd258207b6475e
daniildeli <daniildeli@gmail.com>
2022-10-31 00:16:12 +0200

    Added auth middleware

--- src/models/task.js
+++ src/models/task.js
@@ -4,12 +4,12 @@ const Task = mongoose.model('Task', {
     description: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
     },
     completed: {
         type: Boolean,
-        default: false
-    }
+        default: false,
+    },
 });
 
 // new Task({
