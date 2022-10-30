84dfea537413538655326ada12406eb52a0e3100
daniildeli <daniildeli@gmail.com>
2022-10-30 17:56:18 +0200

    Added async/await to promise-chaining playground

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('635c325a84c918ab56149f34').then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-}).then(count => {
-    console.log(count);
+// Task.findByIdAndDelete('635c325a84c918ab56149f34').then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+// }).then(count => {
+//     console.log(count);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+const deleteTaskAndCount = async id => {
+    await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;   
+}
+
+deleteTaskAndCount('635c4477fd5d3a2724bb3eda', 2).then(count => {
+    console.log('Count:', count);
 }).catch(e => {
-    console.log(e);
-})
+    console.log('Error', e);
+});
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('635c41d4986b5d250f9ca3d1', { age: 1 }).then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-}).then(count => {
-    console.log(count);
+// User.findByIdAndUpdate('635c41d4986b5d250f9ca3d1', { age: 1 }).then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+// }).then(count => {
+//     console.log(count);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+    await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;   
+}
+
+updateAgeAndCount('635c41d4986b5d250f9ca3d1', 2).then(count => {
+    console.log('Count:', count);
 }).catch(e => {
-    console.log(e);
-})
+    console.log('Error', e);
+});
