3a6e7d824e4d1c2d746092bc00a275afb5ef5fb1
daniildeli <daniildeli@gmail.com>
2022-10-31 23:05:52 +0200

    Added endpoint to edit profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -73,7 +73,31 @@ router.get('/users/me', auth, async (req, res) => {
 //     }
 // });
 
-router.patch('/users/:id', async (req, res) => {
+// router.patch('/users/:id', async (req, res) => {
+//     const updates = Object.keys(req.body);
+//     const allowedUpdates = ['name', 'email', 'password', 'age'];
+//     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!', });
+//     }
+
+//     try {
+//         const user = await User.findById(req.params.id);
+//         updates.forEach(update => { user[update] = req.body[update]; });
+//         await user.save();
+//         // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, });
+
+//         if (!user) {
+//             return res.status(404).send();
+//         }
+//         res.send(user);
+//     } catch (e) {
+//         res.status(400).send(e);
+//     }
+// });
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -83,15 +107,9 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id);
-        updates.forEach(update => { user[update] = req.body[update]; });
-        await user.save();
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, });
-
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
+        updates.forEach(update => { req.user[update] = req.body[update]; });
+        await req.user.save();
+        res.send(req.user);
     } catch (e) {
         res.status(400).send(e);
     }
