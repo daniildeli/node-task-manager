b921125379b90678a4562de43205c57274969617
daniildeli <daniildeli@gmail.com>
2022-10-31 22:23:24 +0200

    Added logout logic

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,6 +24,27 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
 // Get all users:
 // router.get('/users', auth, async (req, res) => {
 //     try {
