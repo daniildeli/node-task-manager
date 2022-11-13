743aed836c1751f5e6ece35a9fea45d0dedfad69
daniildeli <daniildeli@gmail.com>
2022-11-14 00:09:37 +0200

    Added unit tests for task APIs

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('./../../src/models/user');
+const Task = require('./../../src/models/task');
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -13,9 +14,46 @@ const userOne = {
     }],
 };
 
-const setupDatabase = async () => {
-    await User.deleteMany();
-    await User(userOne).save();
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+    _id: userTwoId,
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    password: 'myhouse099@@',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId, }, process.env.JWT_SECRET),
+    }],
 };
 
-module.exports = { userOneId, userOne, setupDatabase, };
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First task',
+    completed: false,
+    owner: userOneId,
+};
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second task',
+    completed: true,
+    owner: userOneId,
+};
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third task',
+    completed: true,
+    owner: userTwoId,
+};
+
+const setupDatabase = async () => {
+    await User.deleteMany();
+    await Task.deleteMany();
+    await User(userOne).save();
+    await User(userTwo).save();
+    await Task(taskOne).save();
+    await Task(taskTwo).save();
+    await Task(taskThree).save();
+};
+
+module.exports = { userOneId, userOne, userTwo, taskOne, setupDatabase, };
