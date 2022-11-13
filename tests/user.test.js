43ca3d0e07ba6f986ffd20aa565ad2a0d15d99ae
daniildeli <daniildeli@gmail.com>
2022-11-13 22:39:11 +0200

    Added more unit tests for update user APIs

--- tests/user.test.js
+++ tests/user.test.js
@@ -92,3 +92,37 @@ test('Should not delete account for unauthenitaced user', async () => {
         .send()
         .expect(401);
 });
+
+test('Should uppload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Test Name',
+        })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe('Test Name');
+});
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Test Location',
+        })
+        .expect(400);
+});
